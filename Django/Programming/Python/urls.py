617891fedbfad52e3c92632514e4860fd7273111
Soroush Keyana <35567782+SoroushKeyana@users.noreply.github.com>
2024-01-13 18:15:42 +0100

    Delete Django/Programming directory

--- Django/Programming/Python/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import user_list, index, user_form
-
-urlpatterns = [
-    path('', index, name = 'index'),
-    path('users/', user_list, name='user_list'),
-    path('user-form', user_form, name= 'user_form')
-]
